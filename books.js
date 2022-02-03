10a5307261f10ecdeb9413f1f8a771983d2a3089
Reg-Sherman <reginaxsherman@gmail.com>
2022-02-02 21:11:42 -0600

    finished most functionality

--- books.js
+++ books.js
@@ -4,6 +4,26 @@ const books = [
     title: 'Building API for Fun and Profit',
     author: 'Your Name Here',
   },
+  {
+    id: 0002,
+    title: 'The Hunger Games',
+    author: 'Suzanne Collins',
+  },
+  {
+    id: 0003,
+    title: 'Atomic Habits',
+    author: 'James Clear',
+  },
+  {
+    id: 0004,
+    title: 'Harry Potter and the Chamber of Secrets',
+    author: 'JK Rowling',
+  },
+  {
+    id: 0005,
+    title: 'The Fellowship of the Ring',
+    author: 'JRR Tolkien',
+  },
 ];
 
 module.exports = books;
--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-const books = require('./books');
+const books = require('./books')
+const express = require ('express')
+const app = express()
+
+// Middleware for accessing body data
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/books', (req, res) => {
+    res.send(books)
+})
+
+app.get('/books/:id', (req, res) => {
+    books.forEach(book => {
+        if (book.id === parseInt(req.params.id)) res.json(book)
+    })
+})
+
+app.post('/books', (req, res) => {
+    const newBook = {"id": req.body.id, "title": req.body.title, "author": req.body.author}
+    //the body comes from the request
+    //commonly, we will see this in conjuction with form input 
+    JSON.stringify(newBook)
+    books.push(newBook)
+    res.send(books)
+})
+
+app.delete('/books/:id', (req, res) => {
+    const { id } = req.params
+
+    })
+
+const PORT = 4001
+app.listen(PORT, () => {
+    console.log(`App is listening on port ${PORT}`)
+})
+
